411914f2c7f4ae890714c876d905f59924a404d1
hanif0526 <hanifabdurrahmanarifai@gmail.com>
2026-03-15 11:01:08 +0700

    Add all features

--- app.py
+++ app.py
@@ -1,4 +1,29 @@
 def add(a,b):
-	return a+b
+    return a+b
+    
+def times(a,b):
+    return a*b
+ 
+def minus(a,b):
+    return a-b
+    
+def divide(a,b):
+    return a/b
+    
+print("\nChoose + × - : ")
 
-print(add(1,1))
+choice = input("\nEnter your choice : ")
+
+num1 = float(input("Enter your first number : "))
+num2 = float(input("Enter your second number : "))
+
+if choice == "+":
+    print(f"Result : {add(num1,num2)}")
+elif choice == "×":
+    print(f"Result : {times(num1,num2)}")
+elif choice == "-":
+    print(f"Result : {minus(num1,num2)}")
+elif choice == ":":
+    print(f"Result : {divide(num1,num2)}")
+else:
+    print("Incorrect input")
